8ec02c49c04d2ba8fa18654d80175f74678a1075
milamish <milamish@live.com>
2018-09-03 15:57:39 +0300

    ft: add user signup page ##160232333

---
+++ app/app.py
@@ -0,0 +1,17 @@
+from flask import Flask, jsonify, request
+from flask_restful import Api, Resource
+
+app = Flask(__name__)
+api= Api(app)
+
+users = {}
+
+class Signup(Resource):
+    def post(self):
+        name= request.get_json()['name']
+        emailaddress= request.get_json()['emailaddress']
+        password= request.get_json()['password']
+        username=request.get_json()['username']
+        users.update({username:{"name": name,"emailadress": emailaddress,"password": password}})
+        return jsonify({"name": name},{"username":username})
+api.add_resource(Signup,'/api/v1/auth/signup' )
